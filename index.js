615595825eb4821e71d84f7416ac8aace4d9ab49
BASTIARXPLOIT <75510318+BASTIARXPLOIT@users.noreply.github.com>
2020-12-05 09:37:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'BASTIARXPLOIT BOT☄'; 
+const instagram = 'https://instagram.com/bastiar_404'; 
+const telegram = 'https://t.me/BASTIARXPLOIT'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/c/cyberblackhunter';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
